7ab1a71c9e534691ee4096d1b0af6fe0eb565ff2
Lisa Brown <brown.lisa999@gmail.com>
2023-03-15 11:50:19 -0700

    holidays added

--- script.js
+++ script.js
@@ -8,31 +8,32 @@ let firstOutput = document.querySelector("#first_output");
 let secondOutput = document.querySelector("#second_output");
 let thirdOutput = document.querySelector("#third_output");
 let allDates = document.querySelector("#all_dates")
+let startEnd = document.querySelector("#start_end")
+let calcBtn = document.querySelector("#calc_btn")
+let classDays = document.querySelector("#class_days")
 
 // console.log(startDate, 'start date input')
 // console.log(firstPercent, 'first percent input')
 
 //2023 holidays
 let holidays = {
-    new_years: new Date("January 1, 2024"),
-    juneteenth: new Date("June 19, 2023"),
-    memorial_day: new Date("May 29, 2023"),
-    fourth: new Date("July 4, 2023"),
-    labor_day: new Date("September 4, 2023"),
-    thanksgiving: new Date("November 23, 2023"),
-    christmas: new Date("December 25, 2023"),
+    new_years_24: new Date("January 1, 2024"),
+    juneteenth_23: new Date("June 19, 2023"),
+    memorial_day_23: new Date("May 29, 2023"),
+    fourth_23: new Date("July 4, 2023"),
+    labor_day_23: new Date("September 4, 2023"),
+    thanksgiving_23: new Date("November 23, 2023"),
+    christmas_23: new Date("December 25, 2023"),
+    new_years_23: new Date("January 1, 2023"),
+    juneteenth_22: new Date("June 19, 2022"),
+    memorial_day_22: new Date("May 30, 2022"),
+    fourth_22: new Date("July 4, 2022"),
+    labor_day_22: new Date("September 5, 2022"),
+    thanksgiving_22: new Date("November 24, 2022"),
+    christmas_22: new Date("December 25, 2022"),
 };
 
-//2022 holidays
-// let holidays = {
-//     new_years: new Date("January 1, 2023"),
-//     juneteenth: new Date("June 19, 2022"),
-//     memorial_day: new Date("May 30, 2022"),
-//     fourth: new Date("July 4, 2022"),
-//     labor_day: new Date("September 5, 2022"),
-//     thanksgiving: new Date("November 24, 2022"),
-//     christmas: new Date("December 25, 2022"),
-//   };
+
 
 //days of the week:
 //0 = Sunday
@@ -61,6 +62,40 @@ let isWeekendOrHoliday = function (date) {
     }
     return false;
 };
+
+let generatePlus = function (start, numDays) {
+    //this function takes the start date, the number of class dates needed, and generates 
+    //a list of required class dates
+    let dates_list = [];
+    console.log(start, 'start')
+    startDate = new Date(start);
+
+    let i = 0;
+    let newDay = new Date(startDate.getTime());
+    // Use getTime() to get a copy of the startDate time in milliseconds
+    while (dates_list.length < numDays) {
+        //chatGPT assisted in troubleshooting this loop; it let me know that I needed to use getTime() and turn that into a new Date object using new date()
+        let dateObj = null;
+        if (i == 0) {
+            dateObj = new Date(startDate.getTime());
+            if (isWeekendOrHoliday(dateObj) == false) {
+                dates_list.push(new Date(startDate.getTime()));
+            }
+        } else {
+            newDay.setDate(newDay.getDate() + 1);
+            if (isWeekendOrHoliday(newDay) == false) {
+                dates_list.push(new Date(newDay.getTime()));
+            }
+            //   dateDay = newDay.getDay();
+            //   if (dateDay !== 0 && dateDay !== 6) {
+            //   }
+            // Use getTime() to get a copy of the newDay time in milliseconds
+        }
+        i++;
+    }
+    return dates_list;
+
+}
 
 let generateDates = function (start, end) {
     //generates a list of dates between two dates, including the start and end dates
@@ -100,16 +135,15 @@ let generateDates = function (start, end) {
     console.log(dates_list)
     return dates_list;
 };
-// console.log(
-//   generateDates("January 1, 2023", "January 14, 2023"),
-//   "final console log"
-// );
+
 
 //now I need to find the percentages
 let findPercentages = function (p1, p2, p3, start, end) {
     //using the dates_list, I need to find what indices match the percentages and return them
     //The percentage calculations came from stackoverflow:
     //https://stackoverflow.com/questions/4372902/javascript-calculate-x-of-a-number
+    //aqui also
+    //I can make the number of class dates be an argument,
     let classCalendar = generateDates(start, end);
     let classLength = classCalendar.length;
     let percOne = Math.floor((p1 / 100) * classLength);
@@ -133,6 +167,14 @@ let cleanDate = function (stringDate) {
     return `${stringDate[0]} ${stringDate[1]} ${stringDate[2]}, ${stringDate[3]}`;
 };
 
+calcBtn.addEventListener("click", function () {
+    console.log('yeppers')
+    let startInput = startEnd.value + ' 00:00:01';
+    let startEntered = new Date(startInput);
+
+    console.log(generatePlus(startEntered, classDays.value))
+})
+
 // console.log(findPercentages(25, 45, 75, "January 1, 2023", "January 31, 2023"));
 btn.addEventListener("click", function () {
     let startInput = startDate.value + ' 00:00:01';
@@ -142,9 +184,12 @@ btn.addEventListener("click", function () {
 
     // console.log(startDate.value, "start date input");
     console.log(cleanDate('Wed Mar 01 2023 00:00:01 GMT-0800 (Pacific Standard Time)'), 'clean date')
+    //aqui
+    //get the class dates, when end date known, in variable
+
     let currentClassList = generateDates(startEntered, endEntered)
     let list = document.createElement('ol')
-    for (let i=0; i<currentClassList.length; ++i) {
+    for (let i = 0; i < currentClassList.length; ++i) {
         let listItem = document.createElement('li')
         listItem.innerHTML = cleanDate(currentClassList[i].toString())
         list.appendChild(listItem)
